1c859a38e3ee34f0d5d0b11089d2c206a6ca4fa9
EwenLan <ewen_lan@outlook.com>
2024-12-03 01:07:09 +0800

    optimize ui design

--- timer.xaml.cs
+++ timer.xaml.cs
@@ -1,5 +1,7 @@
 ﻿using System;
+using System.Collections;
 using System.Collections.Generic;
+using System.Data;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -23,15 +25,29 @@ namespace lithium_toolbox
         private Window father;
         private System.Timers.Timer timerCounter = new System.Timers.Timer();
         DispatcherTimer displayRefreshTimer = new DispatcherTimer();
-        DateTime timesUpTime = DateTime.Now;
+        CountDownTimer countDownTimer = new CountDownTimer();
+        DisplayController displayController = new DisplayController();
+
         public Timer(Window father)
         {
             InitializeComponent();
             this.father = father;
-            timerCounter.AutoReset = false;
+            // 显示刷新周期100毫秒
             displayRefreshTimer.Interval = TimeSpan.FromMilliseconds(100);
-            timerCounter.Elapsed += new System.Timers.ElapsedEventHandler(OnTimerTimesUp);
             displayRefreshTimer.Tick += new EventHandler(displayRefreshHandler);
+            displayRefreshTimer.Start();
+            displayController.AddDigit(D0Label);
+            displayController.AddDigit(D1Label);
+            displayController.AddDigit(D2Label);
+            displayController.AddDigit(D3Label);
+            displayController.AddDigit(D4Label);
+            displayController.AddDigit(D5Label);
+            displayController.AddDigit(D6Label);
+            displayController.AddDigit(D7Label);
+            displayController.AddDigit(D8Label);
+            displayController.AddDigit(D9Label);
+            displayController.Show("");
+
         }
 
         private void Window_Closed(object sender, EventArgs e)
@@ -43,34 +59,167 @@ namespace lithium_toolbox
 
         private void M1Button_Click(object sender, RoutedEventArgs e)
         {
-            StartTimerInMinutes(1);
+            this.countDownTimer.AddTimer(1);
         }
-        private void OnTimerTimesUp(object sender, System.Timers.ElapsedEventArgs e)
-        {
-            string content = "Time's Up";
-            string caption = "Time's Up";
-            displayRefreshTimer.Stop();
-            MessageBox.Show(content, caption, MessageBoxButton.OK, MessageBoxImage.Warning);
-        }
-        private void StartTimerInMinutes(int minutes)
-        {
-            var totalSeconds = minutes * 60;
-            timerCounter.Interval = totalSeconds * 1000;
-            timerCounter.Start();
-            timesUpTime = DateTime.Now.AddSeconds(totalSeconds);
-            displayRefreshTimer.Start();
-        }
-        private void displayRefreshHandler(object sender, EventArgs e) {
 
+        private void displayRefreshHandler(object sender, EventArgs e)
+        {
+
+            var display = this.countDownTimer.Display();
+            this.displayController.Show(display);
+        }
+
+        private void ClearButton_Click(object sender, RoutedEventArgs e)
+        {
+            this.countDownTimer.ClearTimer();
+        }
+
+        private void ExitButton_Click(object sender, RoutedEventArgs e)
+        {
+            this.Close();
+        }
+
+        private void Window_MouseLeftButtonDown(object sender, MouseButtonEventArgs e)
+        {
+            try
+            {
+                this.DragMove();
+            }
+            catch { }
+        }
+
+        private void Button_MouseEnter(object sender, MouseEventArgs e)
+        {
+            M1Button.Opacity = 1.0;
+            ClearButton.Opacity = 1.0;
+            ExitButton.Opacity = 1.0;
+        }
+
+        private void M1Button_MouseLeave(object sender, MouseEventArgs e)
+        {
+            M1Button.Opacity = 0.0;
+            ClearButton.Opacity = 0.0;
+            ExitButton.Opacity = 0.0;
+        }
+    }
+
+    public class CountDownTimer
+    {
+        enum State
+        {
+            Init,
+            Running,
+            Stopped,
+        }
+
+        DateTime timesUpTime;
+        State previousState;
+
+        public CountDownTimer()
+        {
+            timesUpTime = DateTime.MinValue;
+            previousState = State.Init;
+        }
+
+        private State GetCurrentState()
+        {
             var now = DateTime.Now;
-            var restTime = timesUpTime - now;
-            var seconds = restTime.Seconds;
-            timeDisplay.Content = seconds.ToString();
+            if (timesUpTime > now)
+            {
+                return State.Running;
+            }
+            TimeSpan stoppedDelay = TimeSpan.FromSeconds(12);
+            var pasedTime = now - timesUpTime;
+            if (pasedTime < stoppedDelay)
+            {
+                return State.Stopped;
+            }
+            return State.Init;
         }
 
-        private void M3Button_Click(object sender, RoutedEventArgs e)
+        private string GetDisplayTimeStr()
+        {
+            var leftTime = timesUpTime - DateTime.Now;
+            var totalSeconds = leftTime.TotalSeconds;
+            var minutes = (int)Math.Floor(totalSeconds / 60);
+            var seconds = (int)Math.Floor(totalSeconds - minutes * 60);
+            if (minutes > 0)
+            {
+                return String.Format("{0,4}", minutes) + ":" + String.Format("{0,2}", seconds);
+            }
+            if (seconds > 1)
+            {
+                return string.Format("{0,2}", seconds) + " Seconds";
+            }
+            return " 1 Second";
+        }
+        public string Display()
+        {
+            switch (this.GetCurrentState())
+            {
+                case State.Running:
+                    previousState = State.Running;
+                    return GetDisplayTimeStr();
+                case State.Stopped:
+                    if (previousState == State.Running)
+                    {
+                        PlayTimesUpNotification();
+                    }
+                    this.previousState = State.Stopped;
+                    
+                    return "Time's Up!";
+                default:
+                    return "";
+            }
+        }
+
+        public void AddTimer(int minutes)
+        {
+            if (timesUpTime < DateTime.Now)
+            {
+                timesUpTime = DateTime.Now;
+            }
+            timesUpTime = timesUpTime + TimeSpan.FromMinutes(minutes);
+        }
+
+        public void ClearTimer()
+        {
+            timesUpTime = DateTime.MinValue;
+        }
+
+        private void PlayTimesUpNotification()
+        {
+            System.Media.SoundPlayer sp = new System.Media.SoundPlayer();
+            sp.SoundLocation = @"./Alarm01.wav";
+            sp.Play();
+        }
+    }
+
+    public class DisplayController
+    {
+        List<Label> digitsDisplay = new List<Label>();
+
+        public DisplayController()
         {
 
+        }
+
+        public void AddDigit(Label label)
+        {
+            this.digitsDisplay.Add(label);
+        }
+
+        public void Show(string content)
+        {
+            var filledLen = digitsDisplay.Count <= content.Length ? digitsDisplay.Count : content.Length;
+            for (int i = 0; i < filledLen; i++)
+            {
+                this.digitsDisplay[i].Content = content[i];
+            }
+            for (int i = filledLen; i < digitsDisplay.Count; i++)
+            {
+                this.digitsDisplay[i].Content = "";
+            }
         }
     }
 }
